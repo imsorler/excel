e64f3099cb69e3b89989f4bd6176937239fe8e02
Eugene Shevchenko <imsorler@gmail.com>
2022-03-25 22:03:46 +0300

    added resizer element

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,13 @@ import { createTable } from './table.template';
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      // listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+    })
+  }
+
   toHTML() {
-    return createTable(50)
+    return createTable(25)
   }
 }
