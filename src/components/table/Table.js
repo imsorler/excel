25c8aff53c225d54e6ba04279752347628dade21
Eugene Shevchenko <imsorler@gmail.com>
2022-03-27 22:30:40 +0300

    createe resizer element

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,16 +1,28 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
 import { createTable } from './table.template';
+import { $ } from '../../core/DOM'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
   constructor($root) {
     super($root, {
-      // listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+      listeners: ['mousedown'],
     })
   }
 
   toHTML() {
     return createTable(25)
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target)
+      const $parent = $resizer.$el.closest('[data-type="resizable"]')
+
+      document.onmousemove = (e) => {
+        console.log(e, $parent);
+      }
+    }
   }
 }
